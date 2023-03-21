af08444d3c7efc660adf98bcfc5ffb24f1d66cfd
khoi <hnak2442001smg4@gmail.com>
2023-03-21 11:30:42 +0700

    done adding header comtainer and navbar  function

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,105 @@
 // var page = document.querySelector("page")
 // var navbar = document.getElementById("nav-bar");
+var nova = document.getElementById("novanoid");
+var mini = document.getElementById("minimalism");
+var crea = document.getElementById("creative");
 
 function menuBtn(){
-    document.getElementById("nav-list-container").classList.toggle("show");
+    var navtoggle= document.getElementById("nav-list-container").classList;
+    if(navtoggle[1] === "show"){
+        navtoggle.add("hide");
+        navtoggle.toggle("show");
+        console.log(navtoggle[0] +" if");
+        console.log(navtoggle[1] +" if");
+        // navtoggle.remove("hide");
+    }
+    else if(navtoggle[1] !=="show"){
+        navtoggle.remove("hide");
+        navtoggle.toggle("show");
+        console.log(navtoggle[0] +" else if");
+        console.log(navtoggle[1] +" else if");
+    }
+    else{
+        // navtoggle.remove("hide")
+        // navtoggle.toggle("show");
+        console.log("error");
+    }
 }
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+function dotOne(){
+    var nova = document.getElementById("novanoid");
+var mini = document.getElementById("minimalism");
+var crea = document.getElementById("creative");
+    nova.classList.add("h1-show");
+    mini.classList.remove("h1-show");
+    crea.classList.remove("h1-show");
+    document.getElementById("dot-1").classList.add("dot-active");
+    document.getElementById("dot-2").classList.remove("dot-active");
+    document.getElementById("dot-3").classList.remove("dot-active");
+}
+
+function dotTwo(){
+    var nova = document.getElementById("novanoid");
+var mini = document.getElementById("minimalism");
+var crea = document.getElementById("creative");
+nova.classList.remove("h1-show");
+mini.classList.add("h1-show");
+crea.classList.remove("h1-show");
+document.getElementById("dot-1").classList.remove("dot-active");
+document.getElementById("dot-2").classList.add("dot-active");
+document.getElementById("dot-3").classList.remove("dot-active");
+}
+
+function dotThree(){
+    var nova = document.getElementById("novanoid");
+var mini = document.getElementById("minimalism");
+var crea = document.getElementById("creative");
+nova.classList.remove("h1-show");
+mini.classList.remove("h1-show");
+crea.classList.add("h1-show");
+document.getElementById("dot-1").classList.remove("dot-active");
+document.getElementById("dot-2").classList.remove("dot-active");
+document.getElementById("dot-3").classList.add("dot-active");
+}
+
+let slideIndex = 0;
+function titleAutoChange(){
+    if(slideIndex==0){
+        dotOne();
+        slideIndex++;
+        console.log("titleAuto 0");
+    }
+    else if(slideIndex==1){
+        dotTwo();
+        slideIndex++;
+        console.log("titleAuto 1");
+    }
+    else if(slideIndex==2){
+        dotThree();
+        slideIndex++;
+        console.log("titleAuto 2");
+    }
+    else{
+        console.log("titleAuto error");
+    }
+    
+    if (slideIndex ==3) {slideIndex = 0}    
+    setTimeout(titleAutoChange, 5000);
+}
+// titleAutoChange();
 (function ($){
 window.onload = (event) => {
+    var nova = document.getElementById("novanoid");
+    nova.classList.add("h1-show");
+    document.getElementById("dot-1").classList.add("dot-active");
+
+    titleAutoChange();
+
+
     var pic = getRandomInt(5) + 1
     console.log(pic);
     var elementHeaderContainer = document.getElementById("header-container");
@@ -31,7 +122,8 @@ window.onload = (event) => {
     else{
         console.log("header img not change");
     }
-    };    
+    };  
+
 window.onscroll = function(){
     //do work
         var scrollTop = window.pageYOffset;
@@ -43,12 +135,12 @@ window.onscroll = function(){
         if (scrollTop >= 147) {
             elementNavbar.classList.add("fix-navbar")
             elementBody.classList.add("noPadBody")
-            console.log("Activated")
+            console.log("navbar stick Activated")
         }
         else {
             elementNavbar.classList.remove("fix-navbar");
             elementBody.classList.remove("noPadBody")
-            console.log("Deactivated")
+            console.log("navbar stick Deactivated")
         }
 
         if (scrollTop >= 570){
